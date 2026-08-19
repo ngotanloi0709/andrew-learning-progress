98144c77adf0db4427bcb427cbc173f39c46724c
ANdrewDUh <andrewngo0609@gmail.com>
2026-08-19 19:11:11 +0700

    sumRec

---
+++ 16082026/facRec.java
@@ -0,0 +1,8 @@
+
+public class facRec {
+    public static int facRec(int n) {
+        if (n == 0 ; n == 1) {
+            return 1;
+        }
+    }
+}
---
+++ 16082026/sumRec.java
@@ -0,0 +1,12 @@
+public class sumRec {
+    public static int sumRec(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sumRec(n - 1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("from 1" + n + "is" + sumRec(n));
+    }
+}
