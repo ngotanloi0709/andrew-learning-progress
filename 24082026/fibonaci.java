000d17e6a2bfac41797b1874761b9688ae7dd7a9
ANdrewDUh <andrewngo0609@gmail.com>
2026-08-30 19:34:58 +0700

    buoi hoc hom nay ngay 30/08/2026

--- 24082026/fibonaci.java
+++ 24082026/fibonaci.java
@@ -1,8 +1,8 @@
 public class fibonaci {
 
     public static int getTheFibonaci(int i) {
-        if (i=0) return 0;
-        if (i=1) return 1;
+        if (i==0) return 0;
+        if (i==1) return 1;
         return getTheFibonaci(i-1) + getTheFibonaci(i-2);
     }
 
@@ -12,7 +12,7 @@ public class fibonaci {
         int result = 0;
         System.out.println("fibonaci numbers less than " + n + " are ");
         
-        while(result <= n) {
+        while(result < n) {
             System.out.println(result);
             i++;
             result = getTheFibonaci(i);
---
+++ 31082026/findMin.java
@@ -0,0 +1,16 @@
+public class findMin {
+    public static int findMax(int[] array) { //tao ham ten la findmax
+        int max = array[0]; //cho so max hien tai la so dau tien
+        for(int i = 0; i < array.length; i++) {//cho i = 0, i se nho hon length cua array roi cong vo sau moi vong lap
+            if(array[i] > max) {//neu so ki tu i lon hon so max hien tai
+                max = array[i]; // max se la so ki tu i hien tai
+            }
+        }
+        return max; // return max
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 2, 6, 7, 1, 8, 9}; //day so cua minh da duoc tao ra
+        System.out.println(findMax(array));// in ra so lon nhat
+    }
+}
