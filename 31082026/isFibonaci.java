09ed7f1689e79fc85d0abca06248898e250005ce
ANdrewDUh <andrewngo0609@gmail.com>
2026-08-31 20:02:09 +0700

    bai isfibonaci

---
+++ 31082026/isFibonaci.java
@@ -0,0 +1,16 @@
+public class isFibonaci {
+    public static boolean isFibonaci(int n) {
+        if (n < 0) return false;
+        int a = 0, b = 1;
+        if (n == a | n == b) return true;
+        
+        int c = a + b;
+        while (c < n) {
+            if (c == n) return true;
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return false;
+    }
+}
